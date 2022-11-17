aeec8145dc9fa864e93fe05e516ed7049d152e9f
JacW <94559281+xKeiv@users.noreply.github.com>
2022-11-17 23:13:00 +0100

    Added Navbar

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
 
 const Navbar = () => {
   return (
-    <div>
-        {/*  */}
-    </div>
+    <Nav>
+      <Logo href="#">
+        Heph Video Player
+      </Logo>
+      <Menu>
+        <Link href="#">About</Link>
+        <Link href="#">Change</Link>
+        <Link href="#">GitHub</Link>
+      </Menu>
+    </Nav>
   )
 }
+
+//Styling
+
+const Nav = styled.div`
+padding: 0 2rem;
+
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-wrap: wrap;
+
+background: #252525;`
+
+const Menu = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+position: relative;
+height: 1.3rem;
+`
+const Logo = styled.a`
+padding: 1rem 0;
+text-decoration: none;
+color: #f5cb6e;
+font-size: 1.3rem;
+font-weight: 700;`
+
+const Link = styled.a`
+padding: 0 2rem;
+cursor: pointer;
+text-decoration: none;
+color: #DCDADA;
+transition: all 0.5s ease-in-out;
+
+
+&:hover {
+  color: #f5cb6e;
+  border-bottom: 1px solid #f5cb6e;
+}`
 
 export default Navbar
